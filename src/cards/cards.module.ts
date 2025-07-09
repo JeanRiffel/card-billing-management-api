39f53d652b27a0ccd1190945bb19b7eb650fc152
user <jeanriffel1@gmai.com>
2025-07-09 20:36:16 -0300

    feat: creating card endpoint

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [CardsService],
+  providers: [CardsService, PrismaService],
   controllers: [CardsController]
 })
 export class CardsModule {}
