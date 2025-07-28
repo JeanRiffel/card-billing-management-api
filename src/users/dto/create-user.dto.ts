55041a68e2d1f7afd5f0fc99ac2161a5cbd48dbd
jean <jeanriffel1@gmail.com>
2025-07-28 16:20:47 -0300

    docs: adding create user swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'john@email.com'})
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '1234'})
   @IsNotEmpty()  
   password: string;
 
+  @ApiProperty({example: 'John Woo'})
   @IsNotEmpty()
   name: string;
 }
