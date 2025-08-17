c29585bd722ffeab2cd1c882c9f825acf4c6f8e9
jean <jeanriffel1@gmail.com>
2025-08-17 16:29:11 -0300

    feat(purchases): creating purchases endpoint - GET method

--- src/purchases/purchases.service.ts
+++ src/purchases/purchases.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PurchasesService {}
+export class PurchasesService {
+  constructor(private prisma: PrismaService){}
+
+  async findById(id: string){
+    const purchases = await this.prisma.purchase.findUnique({
+      where: { id },
+    });
+
+    if (!purchases){
+      return null;
+    }
+
+    return purchases
+  }  
+
+}
